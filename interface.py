5857218f7960201b0ea0a17ae3c078aec177707d
Raphaël Bardini <winclean.raphaelbardini@gmail.com>
2022-05-08 18:27:41 +0200

    Petites améliorations à droite à gauche

--- interface.py
+++ interface.py
@@ -13,17 +13,20 @@ def creerFenetre():
     
     INTROUVABLE = "(introuvable)"
     
+    # Définition de la marge (padding) extérieure à 10 pixels.
+    PAD = 10
+    
     dicLabels = {}
     
     def selectionnerFichier():
-        filename = tk.filedialog.askopenfilename()
-        signature = main.obtenirSignature(filename)
+        nomFichier = tk.filedialog.askopenfilename()
+        signature = main.obtenirSignature(nomFichier)
 
         def configurerLabel(nom, texteAAjouter):
             """ Met la propriété text du label de dicLabels[nom] à la valeur nom+texteAAjouter. """
             dicLabels[nom].configure(text = nom+texteAAjouter)
         
-        configurerLabel(EXPLORATEUR, filename)
+        configurerLabel(EXPLORATEUR, nomFichier)
         
         # Si la signature n'a pas été trouvée (le DataFrame est vide)
         if signature.empty:
@@ -38,25 +41,22 @@ def creerFenetre():
     
     root = tk.Tk()
     root.title("Détecteur de signature de fichier") # titre de la fenêtre
-    root.minsize(219, 180) # taille minimum en pixels
+    root.minsize(237, 180) # taille minimum en pixels
     root.config(bg="white") # couleur de fond : blanc
     
-    # Définition de la marge (padding) extérieure à 10 pixels.
-    PAD = 10
-    
-    button_explore = ttk.Button(text = "Parcourir", command = selectionnerFichier, padding=(3,3,3,3))
-    button_explore.pack(padx = PAD, pady = PAD, side=tk.LEFT, anchor = tk.NW)
+    boutonParcourir = ttk.Button(text = "Parcourir", command = selectionnerFichier, padding=(3,3,3,3))
+    boutonParcourir.pack(padx = PAD, pady = PAD, side=tk.LEFT, anchor = tk.NW)
     
     
-    def update_wraplength(event):
+    def updateWraplength(event):
         """ Sous-fonction qui définit la propriété wraplength d'un contrôle à la largeur de celui-ci.
             Cela garantit un comportement cohérent de retour à la ligne automatique. """
-        event.widget.configure(wraplength=event.width)
+        event.widget.configure(wraplength=event.width-6)
     
     def ajouterLabel(texte):
         """ Fonction locale qui permet d'éviter la répétition du code de création de labels. """
         label = ttk.Label(root, text=texte, borderwidth=2, relief="groove", padding=(3,3,3,3)) # Création du label
-        label.bind("<Configure>", update_wraplength) # Bind de l'évènement de changement de taille
+        label.bind("<Configure>", updateWraplength) # Bind de l'évènement de changement de taille
         dicLabels[texte] = label # Ajout au dictionnaire des labels
         label.pack(padx = PAD, pady = PAD, fill=tk.BOTH, expand=1) # Pack du label
     
--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import interface
 """ Nom du fichier CSV de base de données de signatures. """
 NOM_CSV = "signatures.csv"
 
-def lireHexadecimal(nomFichier: str, nbCaracteres: int):
-    assert nbCaracteres > 0, "Le nombre de caractères à lire doit être un entier positif non nul"
-    """ Lit en héxadécimal dans un fichier un certain nombre de caractères.
+def lireHexadecimal(nomFichier: str, nbOctets: int):
+    assert nbOctets >= 0, "Le nombre d'octets à lire doit être positif"
+    """ Lit en héxadécimal dans un fichier un certain nombre d'octets.
     Source : https://stackoverflow.com/a/34687617 """
     with open(nomFichier, 'rb') as f:
-        hexdata = f.read().hex()
-    return hexdata[0:nbCaracteres]
+        return f.read(nbOctets).hex()
 
 def obtenirLongueurPlusLongueSignature():
     """ Retourne la longueur de la plus longue signature enregistrée dans la base de données. """
@@ -42,13 +41,14 @@ def rechercherSignature(possibleSignature: str):
 
 
 def obtenirSignature(nomFichier):
-    return rechercherSignature(lireHexadecimal(nomFichier, obtenirLongueurPlusLongueSignature()))
+    return rechercherSignature(lireHexadecimal(nomFichier, obtenirLongueurPlusLongueSignature()//2))
 
 
 def test():
     """ Test de la fonction de lecture hexadécimale et de l'algorithme de recherche de signature """
     print("Test en cours...")
-    hexdata = lireHexadecimal("fichier_test", obtenirLongueurPlusLongueSignature())
+    hexdata = lireHexadecimal("fichier_test", obtenirLongueurPlusLongueSignature()//2)
+    print(hexdata)
     assert hexdata == "4d5a900003000000", "La fonction lireHexadecimal a retourne de mauvaises donnees."
     assert rechercherSignature(hexdata).Signature.iloc[0] == "4d5a", "La fonction trouverSignature n'a pas trouve la signature."
     print("Test reussi.")
